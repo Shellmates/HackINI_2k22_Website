1887217783bcf91a64bf3e612048d06e752232fb
Walid Berrouk <jw_berrouk@esi.dz>
2022-05-15 20:05:32 +0100

    adding all sections, first deployement

--- scripts/Countdown.js
+++ scripts/Countdown.js
@@ -1,17 +1,19 @@
 const days = document.getElementsByClassName("daysTXT")[0];
 const hours = document.getElementsByClassName("hoursTXT")[0];
 const minutes = document.getElementsByClassName("minutesTXT")[0];
+const seconds = document.getElementsByClassName("secondsTXT")[0];
 const currentyear = new Date().getFullYear();
-const eventTime = new Date(`April 21 2022 20:00:00`);
+const newYearTime = new Date(`June 26 2022 17:00:00`);
 
 function updateCountdowntime() {
   const currentTime = new Date();
-  const diff = eventTime - currentTime;
+  const diff = newYearTime - currentTime;
   const d = Math.floor(diff / (1000 * 60 * 60 * 24));
   const h = Math.floor(diff / (1000 * 60 * 60)) % 24;
   const m = Math.floor(diff / (1000 * 60)) % 60;
+  const s = Math.floor(diff / 1000) % 60;
 
-  if ( m < 0 ) {
+  if ( s < 0 ) {
     days.innerText =  "00" ;
     hours.innerText = "00" ;
     minutes.innerText =  "00" ;
@@ -20,6 +22,7 @@ function updateCountdowntime() {
     days.innerText = d < 10 ? "0" + d : d;
     hours.innerText = h < 10 ? "0" + h : h;
     minutes.innerText = m < 10 ? "0" + m : m;
+    seconds.innerText = s < 10 ? "0" + s : s;
   }
 }
 
