c9dd95debdf827e64ddb9340254489f8d230d389
Walid Berrouk <jw_berrouk@esi.dz>
2022-05-16 22:47:35 +0100

    adding typing animation, navbar fixed

---
+++ scripts/waypoints.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+    $('.typing-demo-event').waypoint(function() {
+    
+    $('.typing-demo-event').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+    $('.typing-demo-1').waypoint(function() {
+    
+    $('.typing-demo-1').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+    
+    $('.typing-demo-2').waypoint(function() {
+    
+    $('.typing-demo-2').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+    $('.typing-demo-3').waypoint(function() {
+    
+    $('.typing-demo-3').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+    $('.typing-demo-4').waypoint(function() {
+    
+    $('.typing-demo-4').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+    $('.typing-demo-5').waypoint(function() {
+    
+    $('.typing-demo-5').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+    $('.typing-demo-6').waypoint(function() {
+    
+    $('.typing-demo-6').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+    $('.typing-demo-7').waypoint(function() {
+    
+    $('.typing-demo-7').css({
+    
+    animation: "typing 2s steps(22), blink .5s step-end infinite alternate",
+    
+    opacity: "1"
+    
+    });
+    
+    }, { offset: '85%' });
+
+});
