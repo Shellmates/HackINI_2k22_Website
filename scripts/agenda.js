a616ca5234ccedc745b6f0dc38c1ecde4ac6dd59
Abderaouf Louggani <ja_louggani@esi.dz>
2022-05-23 21:53:41 +0100

    agenda + speakers

--- scripts/agenda.js
+++ scripts/agenda.js
@@ -1,55 +1,75 @@
 const days = [
     {
         day: "Day 01",
-        date: "May 19th",
+        date: "May 26th",
         activities: [
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "17:00",
+                activity: "Check-in"
             },
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "18:30",
+                activity: "Opening ceremony"
             },
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "19:30",
+                activity: "CTF Start"
+            },
+            {
+                time: "21:00",
+                activity: "Dinner"
             }
         ]
     },
     {
         day: "Day 02",
-        date: "May 20th",
+        date: "May 27th",
         activities: [
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "07:30",
+                activity: "Breakfast"
             },
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "09:00 - 10:30",
+                activity: "Digital forensics"
             },
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "11:00 - 12:30",
+                activity: "Docker security"
             },
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "12:30",
+                activity: "Lunch break"
+            },
+            {
+                time: "14:30 - 16:30",
+                activity: "Cryptographic Vulnerabilities"
+            },
+            {
+                time: "16:00",
+                activity: "Coffee Break"
+            },
+            {
+                time: "16:30 - 18:00",
+                activity: "Intro to Binary Exploitation"
+            },
+            {
+                time: "21:00",
+                activity: "Dinner"
             }
         ]
     },
     {
         day: "Day 03",
-        date: "May 21th",
+        date: "May 28th",
         activities: [
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "07:30",
+                activity: "CTF end, Breakfast"
             },
             {
-                time: "00:00",
-                activity: "Conferance/Workshop"
+                time: "08:30",
+                activity: "Closing ceremony"
             }
         ]
     }
@@ -64,7 +84,7 @@ days.map(day => {
 
     const titleContainer = document.createElement("div")
 
-    titleContainer.classList = "w-36"
+    titleContainer.classList = "w-28 md:w-36"
 
     titleContainer.innerHTML = `
         <h4 class="uppercase font-bold title-gradient text-lg md:text-3xl">${day.day}</h4>
@@ -77,13 +97,13 @@ days.map(day => {
 
     const activitiesContainer = document.createElement("div")
 
-    activitiesContainer.classList = "md:text-lg lg:text-xl xl:text-2xl grad flex flex-col gap-y-2"
+    activitiesContainer.classList = "md:text-lg lg:text-xl xl:text-2xl grad flex flex-col gap-y-2 w-3/5 md:w-auto"
 
     day.activities.map(activity => {
         const activityContainer = document.createElement("div")
 
         activityContainer.innerHTML = `
-            ${activity.time} Activity : ${activity.activity}
+            ${activity.time} : ${activity.activity}
         `
         activitiesContainer.append(activityContainer)
     })
